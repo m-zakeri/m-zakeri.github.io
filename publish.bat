10d1708194e53b1dcf9551148244229f2252eeca
m-zakeri <m-zakeri@live.com>
2019-02-22 19:02:31 +0330

    logo deleted

--- publish.bat
+++ publish.bat
@@ -2,7 +2,7 @@ git add .
 git commit -a -m %1
 git push -u origin pelican
 REM pelican content -o output -s pelicanconf.py
-pelican content -o output -s publishconf.py
+pelican content -o output -s pelicanconf.py
 ghp-import output -r origin -b master
 git push origin master
 git checkout pelican
