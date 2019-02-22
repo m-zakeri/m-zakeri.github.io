27c861ed3ee952081114f3028edfc7adb193f45a
m-zakeri <m-zakeri@live.com>
2019-02-22 16:01:04 +0330

    settings changed

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,10 +4,25 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Morteza'
 SITENAME = 'Morteza ZAKERI'
-SITEURL = ''
+SITEURL = 'http://localhost:8000'
+SITETITLE = 'Morteza ZAKERI Personal Webpage'
+SITESUBTITLE = 'Ph.D. Student, Computer Engineering'
+SITEDESCRIPTION = 'Iran University of Science and Technology'
+
+PYGMENTS_STYLE = 'monokai'
 
 PATH = 'content'
 
+# Copyrigth
+CC_LICENSE = {
+    'name': 'All rights reserved.',
+    'version': '1.0',
+    'slug': 'Morteza ZAKERI'
+}
+
+COPYRIGHT_YEAR = datetime.now().year
+
+# Time and date
 TIMEZONE = 'Asia/Tehran'
 
 DEFAULT_LANG = 'English'
@@ -21,21 +36,35 @@ AUTHOR_FEED_RSS = None
 TYPOGRIFY = True
 
 # Blogroll
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_CATEGORIES_ON_MENU = True
+
+MENUITEMS = ('Home', '/index.html'),
+			('Archives', '/archives.html'),
+            ('Tags', '/tags.html'),)
+
 LINKS = (('IUST Reverse Engineering Laboratory', 'http://parsa.iust.ac.ir/reverse-engineering-lab/'),
          ('Micropedia', 'http://micropedia.ir'),
          ('IUST Personal Page', 'http://webpages.iust.ac.ir/morteza_zakeri/'),
-         ('GitHub', 'https://github.com/m-zakeri/'),)
+		 
+		 )
 
 # Social widget
-SOCIAL = (('LinkedIn', 'https://www.linkedin.com/in/mortazazakeri/'),
-          ('Twitter', 'https://twitter.com/_zakeri_'),)
+GITHUB_URL = 'https://github.com/m-zakeri/'
+TWITTER_USERNAME = '_zakeri_'
+SOCIAL = (('linkedin', 'https://www.linkedin.com/in/mortazazakeri/'),
+          ('twitter', 'https://twitter.com/_zakeri_'),
+		  ('github', 'https://github.com/m-zakeri/'),
+		  ('github', 'https://github.com/mortazazakeri/'),
+		  ('orcid', 'https://orcid.org/0000-0003-4289-0606'))
 
 DEFAULT_PAGINATION = 5
 
 STATIC_PATHS = ['_img', '_pdf', '_css']
 THEME = 'Flex'
 FAVICON = '_img/favicon.ico'
-#CUSTOM_CSS = '_css/custom.css'
+PROFILE = '_img/morteza_zakeri_lattary600x600.png'
+CUSTOM_CSS = '_css/custom.css'
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
